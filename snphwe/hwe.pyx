5a3ef910082b4411acd40f99501dbb304ba42734
Jeremy McRae <jmcrae@illumina.com>
2020-06-11 16:03:38 -0700

    account for genotype uncertainty

--- snphwe/hwe.pyx
+++ snphwe/hwe.pyx
@@ -3,4 +3,4 @@ cdef extern from "snp_hwe.h" namespace 'snphwe':
     double SNPHWE(long, long, long) except +ValueError
 
 def snphwe(obs_hets, obs_hom1, obs_hom2):
-    return SNPHWE(obs_hets, obs_hom1, obs_hom2)
+    return SNPHWE(round(obs_hets), round(obs_hom1), round(obs_hom2))
--- tests/test_hwe.py
+++ tests/test_hwe.py
@@ -23,3 +23,17 @@ class TestHWE(unittest.TestCase):
         ''' check snphwe doesn't give errors with large sample sizes
         '''
         self.assertEqual(snphwe(200000, 200000, 200000), 0.0)
+    
+    def test_snphwe_uncertain_genotypes(self):
+        ''' check uncertain genotypes give correct p-values
+        
+        Imputed genotypes need HWE checks too. I checked this against the method
+        from:
+        Shriner D. (2011) Genet Epidemiol. 35:632‐637. doi:10.1002/gepi.20612
+        Approximate and exact tests of Hardy-Weinberg equilibrium using uncertain genotypes.
+        
+        Instead of running the full exact test from that, I simply round the
+        genotype totals before calling the underlying c++ function. This should
+        still be reasonably accurate and fast.
+        '''
+        self.assertAlmostEqual(snphwe(4989.99999, 494999.999, 9.9999999), 0.570223198305)
